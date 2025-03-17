a1aa4f2696626d41ffaf317a390c3667d097e393
righ1113 <34955220+righ1113@users.noreply.github.com>
2025-03-17 22:20:19 +0900

    first commit

---
+++ emehcs.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+# ・以下は1回だけおこなう
+# rbenv で Ruby 3.3.5(じゃなくてもいい) を入れる
+# $ gem install bundler
+# $ cd emehcs3
+# $ bundle install --path=vendor/bundle
+
+# ・実行方法
+# $ cd emehcs3
+# $ RUBY_THREAD_VM_STACK_SIZE=100000000 bundle exec ruby emehcs.rb
+# > [ctrl]+D か exit で終了
+
+require './lib/const'
+require './lib/parse2_core'
+require './lib/repl'
+
+# EmehcsBase クラス
+class EmehcsBase
+  include Const
+  def initialize = (@env = { 'true' => 'true', 'false' => 'false' }; @stack = [])
+
+  private
+
+  # スタックから count 個の要素を取り出して、評価する(実際に値を使用する前段階)
+  def common(count)
+    values = init_common count
+    values.map! { |y| func?(y) ? parse_run(y) : y }
+    count == 1 ? values.first : values # count が 1 なら最初の要素を返す
+  end
+
+  # if と &&
+  def my_if(count = 3)
+    values = init_common count
+    else_c = Delay.new { count == 3 ? parse_run([values[2]]) : 'false' }
+    @stack.push parse_run([values[0]]) == 'true' ? parse_run([values[1]]) : else_c.force
+  end
+end
+
+# Emehcs クラス 相互に呼び合っているから、継承しかないじゃん
+class Emehcs < EmehcsBase
+  include Parse2Core
+  def run(str_code) = (@stack = []; run_after(parse_run(parse2_core(str_code)).to_s))
+
+  # メインルーチンの改善
+  def parse_run(code)
+    case code
+    in [] then @stack.pop
+    in [x, *xs] # each_with_index 使ったら、再帰がよけい深くなった
+      case x
+      in Integer then @stack.push x
+      in Array   then @stack.push parse_array  x, xs.empty?
+      in String  then my_ack_push parse_string x, xs.empty?
+      in Symbol  then nil # do nothing
+      else            raise ERROR_MESSAGES[:unexpected_type]
+      end
+      parse_run xs
+    end
+  end
+
+  private
+
+  # (1) Array のとき、code の最後かつ関数だったら実行する、でなければ実行せずに積む
+  def parse_array(x, em) = em && func?(x) ? parse_run(x) : x
+
+  # String のとき
+  def parse_string(x, em, name = x[1..], db = [x, @env[x]], b = em && func?(@env[x]),
+                   co = Delay.new { Const.deep_copy(@env[x]) },
+                   pr = Delay.new { pop_raise })
+    db.each do |y|
+      if EMEHCS_FUNC_TABLE.key? y       # primitive関数 の実行
+        em ? send(EMEHCS_FUNC_TABLE[y]) : @stack.push(y)
+        return nil
+      end
+    end
+    if x[-2..] == SPECIAL_STRING_SUFFIX # 純粋文字列 :s
+      x
+    elsif x[0] == FUNCTION_DEF_PREFIX   # 関数定義
+      @env[name] = pop_raise; nil
+    elsif x[0] == VARIABLE_DEF_PREFIX   # (3) 変数定義のときは、Array を実行する
+      @env[name] = func?(pr.force) ? parse_run(pr.force) : pr.force; nil
+    elsif @env[x].is_a?(Array)          # (2) この時も code の最後かつ関数なら実行する
+      ________________________ = b ? parse_run(co.force) : co.force
+    else                                # x が変数名
+      @env[x]
+    end
+  end
+end
+
+# メイン関数としたもの
+if __FILE__ == $PROGRAM_NAME
+  emehcs = Emehcs.new
+  repl = Repl.new emehcs
+  repl.prelude
+  repl.repl
+end
---
+++ test/emehcs_test.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+# ・実行方法
+# $ cd emehcs3
+# $ ruby test/emehcs_test.rb
+
+require 'minitest/autorun'
+require './emehcs'
+
+class EmehcsTest < Minitest::Test
+  def test_case1
+    emehcs = Emehcs.new
+    code1  = [1, 2, '+']
+    code2  = [[3, 7, '+'], 1, '+']
+    code3  = [2, [3, 7, '+'], '+']
+    code4  = [99, '=x', 'x']
+    code5  = [['=x', [1, 8, '+']], '>fact', 4, 'fact']
+    code6  = [['=x', 'x'], '>id', 4, 'id']
+    code7  = [['=x', [1, 'x', '+']], '>fact', 4, 'fact']
+    code8  = [['=x', [[['x', 1, '-'], 'fact'], 'x', '*'], 1, 'true', '?'], '>fact', 4, 'fact']
+    code9  = [['=x', [[['x', 1, '-'], 'fact'], 'x', '*'], 1, ['x', 1, '<'], '?'], '>fact', 4, 'fact']
+    code10 = [5, 'fact']
+    code11 = [[1, 2, 3, :q], 'id']
+    code12 = [[1, 2, 3, :q], '=dat', 'dat', 'id']
+
+    assert_equal 3,                    (emehcs.parse_run code1)
+    assert_equal 11,                   (emehcs.parse_run code2)
+    assert_equal 12,                   (emehcs.parse_run code3)
+    assert_equal 99,                   (emehcs.parse_run code4)
+    assert_equal 9,                    (emehcs.parse_run code5)
+    assert_equal 4,                    (emehcs.parse_run code6)
+    assert_equal 5,                    (emehcs.parse_run code7)
+    assert_equal 1,                    (emehcs.parse_run code8)
+    assert_equal 24,                   (emehcs.parse_run code9)
+    assert_equal 120,                  (emehcs.parse_run code10)
+    assert_equal [1, 2, 3, :q],        (emehcs.parse_run code11)
+    assert_equal [1, 2, 3, :q],        (emehcs.parse_run code12)
+  end
+
+  def test_case2
+    emehcs = Emehcs.new
+    code22 = '[] [3] =='
+    code24 = '5 (5 +) (2 *)'
+    code25 = '((4 3 false ?) 2 false ?) 1 false ?'
+    code26 = '5 (5 +) (2 *) (3 -)'
+    code27 = '"aaa    aaa a   a"'
+
+    assert_equal 'false',              (emehcs.run code22)
+    assert_equal '20',                 (emehcs.run code24)
+    assert_equal '4',                  (emehcs.run code25)
+    assert_equal '17',                 (emehcs.run code26)
+    assert_equal '"aaa    aaa a   a"', (emehcs.run code27)
+  end
+
+  def test_case3
+    emehcs = Emehcs.new
+    code34 = '"abcde" 2 !!'
+    code35 = '"abcde" length'
+    code36 = '88 chr'
+    code37 = '[1 2 3] 1 100 up_p'
+    code38 = '0 99 ((3 3 ==) true &&) ?'
+
+    assert_equal '"c"',                (emehcs.run code34)
+    assert_equal '5',                  (emehcs.run code35)
+    assert_equal 'X',                  (emehcs.run code36)
+    assert_equal '[1 102 3]',          (emehcs.run code37)
+    assert_equal '99',                 (emehcs.run code38)
+  end
+end
