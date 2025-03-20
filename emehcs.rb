1b1a8412c95765793dde4e49d9afdad1a4962fc1
righ1113 <34955220+righ1113@users.noreply.github.com>
2025-03-21 03:02:49 +0900

    25/03/21 整頓13

--- emehcs.rb
+++ emehcs.rb
@@ -44,14 +44,19 @@ class Emehcs < EmehcsBase
       end; parse_run xs
     end
   end
-  private def parse_array(x, em) = (p "code:#{x}, em:#{em}"; em && func?(x) ? parse_run(x) : x)
+  private def parse_array(x, em) = em && func?(x) ? parse_run(x) : x
   private def parse_string(x, em, name = x[1..], db = [x, @env[x]], co = Const.deep_copy(@env[x]))
     db.each { |y| (em ? send(EMEHCS_FUNC_TABLE[y]) : @stack.push(y); return nil) if EMEHCS_FUNC_TABLE.key? y }
     if x[-2..] == SPECIAL_STRING_SUFFIX then x # 純粋文字列 :s
-    elsif x[0] == FUNCTION_DEF_PREFIX   then @env[name] = pop_raise; p "n:#{name}, e:#{@env[name]}"; nil # 関数束縛
-    elsif x[0] == VARIABLE_DEF_PREFIX   then @env[name] = parse_array(pop_raise, true); em_n_nil(em, name)
-    elsif @env[x].is_a?(Array)          then              parse_array co, em # code の最後かつ関数なら実行する
-    elsif true                          then @env[x]                         # x が変数名
+    elsif x[0] == FUNCTION_DEF_PREFIX          # 関数束縛
+      @env[name] = parse_array(pop_raise, false)
+      # p "n:#{name}, e:#{@env[name]}"
+      nil
+    elsif x[0] == VARIABLE_DEF_PREFIX
+      @env[name] = parse_array(pop_raise, true)
+      em_n_nil(em, name)
+    elsif @env[x].is_a?(Array)          then parse_array co, em # code の最後かつ関数なら実行する
+    elsif true                          then @env[x]            # x が変数名
     end
   end
 end
